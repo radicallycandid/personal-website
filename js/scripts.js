3f2889ea0f669715c2108f8727b990779c8dfa80
radicallycandid <77696430+radicallycandid@users.noreply.github.com>
2024-07-19 23:04:28 -0300

    Fix conversion of Md to HTML before rendering posts

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,26 @@ function fetchPosts() {
                 postElement.innerHTML = `
                     <h3>${post.title}</h3>
                     <p>${post.excerpt}</p>
-                    <a href="${post.url}">Read more</a>
+                    <a href="${post.url}" class="read-more" data-post-url="${post.url}">Read more</a>
                 `;
                 postsDiv.appendChild(postElement);
             });
+            addReadMoreEventListeners();
         });
+}
+
+function addReadMoreEventListeners() {
+    const readMoreLinks = document.querySelectorAll('.read-more');
+    readMoreLinks.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            const postUrl = event.target.getAttribute('data-post-url');
+            fetch(postUrl)
+                .then(response => response.text())
+                .then(markdown => {
+                    const contentDiv = document.getElementById('content');
+                    contentDiv.innerHTML = marked.parse(markdown);
+                });
+        });
+    });
 }
